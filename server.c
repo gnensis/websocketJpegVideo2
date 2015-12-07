ec472668f7141e83bc067d284029c4618225f349
CHANG FU CHIAO <zector1030@gmail.com>
2015-12-07 12:11:33 +0800

    split http header

--- server.c
+++ server.c
@@ -3,66 +3,105 @@
 #include<sys/socket.h>
 #include<arpa/inet.h> //inet_addr
 #include<unistd.h>    //write
- 
+
+int isAsciiArgs(char *p, int len)
+{
+	for (; len > 0; len--, p++) {
+		if (*p == ':') 
+			return 1;
+	}
+	return 0;
+}
+int argLength(char *p, int last)
+{
+    	int i;
+	for (i = 0; *(p+i) != '\r' && i <= last; i++) ;
+	return i;
+}
+#define HEX 0
+#define ASCII 1
+void printArgs(char *p, int n, int type)
+{
+	for (; n > 0; n--, p++) {
+	    if (type == HEX)
+		printf("0x%x ", *p);
+	    else
+	    	putchar(*p);
+	}
+	printf("\n");
+}
 int main(int argc , char *argv[])
 {
     int socket_desc , client_sock , c , read_size;
     struct sockaddr_in server , client;
-    char client_message[2000];
+    char msg[1024] = {0};
+    char reply[1024] = {0};
      
-    //Create socket
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
-    if (socket_desc == -1)
-    {
-        printf("Could not create socket");
+    if (socket_desc == -1) {
+        perror("socket() err\n");
+	return 0;
     }
-    puts("Socket created");
+    printf("socket created\n");
      
-    //Prepare the sockaddr_in structure
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons( 8888 );
+    server.sin_port = htons(9999);
      
-    //Bind
-    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        //print the error message
-        perror("bind failed. Error");
-        return 1;
+    if (bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0) {
+        perror("bind() err\n");
+        return 0;
     }
-    puts("bind done");
+    printf("bind done\n");
      
-    //Listen
-    listen(socket_desc , 3);
-     
-    //Accept and incoming connection
-    puts("Waiting for incoming connections...");
+    listen(socket_desc , 1);
+    printf("Waiting for incoming connections...");
     c = sizeof(struct sockaddr_in);
      
-    //accept connection from an incoming client
     client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-    if (client_sock < 0)
-    {
-        perror("accept failed");
-        return 1;
+    if (client_sock < 0) {
+        perror("accept() err\n");
+        return 0;
     }
-    puts("Connection accepted");
+    printf("Connection accepted\n");
      
-    //Receive a message from client
-    while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
-    {
+    strcpy(reply, "HTTP/1.1 101 Switching Protocols\r\n");
+
+    while((read_size = recv(client_sock, msg, 1024, 0)) > 0 ) {
         //Send the message back to client
-        write(client_sock , client_message , strlen(client_message));
+        //write(client_sock , client_message , strlen(client_message));
+	printf("client:\n%s\n", msg);
+	int i = 0, n = 0;
+	while (i < read_size) {
+	    n = argLength(msg+i, read_size-i);
+	    if (strncmp(msg+i, "Upgrade:", 8) == 0 ||
+	    	strncmp(msg+i, "Connection:", 11) == 0 ||
+	    	strncmp(msg+i, "Sec-WebSocket-Protocol:", 23) == 0) {
+//		strncat(reply, msg+i, j);
+		printArgs(msg+i, n, ASCII);
+	    }
+	    else if (strncmp(msg+i, "Sec-WebSocket-Key:", 18) == 0) {
+		printf("**");
+		printArgs(msg+i, n, ASCII);
+	    }
+	    else if (isAsciiArgs(msg+i, n)) {
+		printf("--");
+		printArgs(msg+i, n, ASCII);
+	    }
+	    else {
+		printf("--");
+		printArgs(msg+i, n, HEX);
+	    }
+	    fflush(stdout);
+	    i += (n+2);
+	}
     }
      
-    if(read_size == 0)
-    {
-        puts("Client disconnected");
-        fflush(stdout);
+    if(read_size == 0) {
+        perror("Client disconnected\n");
     }
-    else if(read_size == -1)
-    {
-        perror("recv failed");
+    else if(read_size == -1) {
+        perror("recv failed\n");
     }
      
     return 0;
